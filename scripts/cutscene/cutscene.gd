a2473561229c45a2172da657287f1685e6dfbaca
Lucas L. Lopes <lucasrodrigoleallopes2014@gmail.com>
2025-06-22 19:41:38 -0300

    Add cutscene

--- scripts/cutscene/cutscene.gd
+++ scripts/cutscene/cutscene.gd
@@ -1,9 +1,9 @@
 extends Node
 
 func _ready():
-        var video = get_node("VideoStreamPlayer")
-        video.finished.connect(self._on_video_finished)
-        video.play()
+		var video = get_node("VideoStreamPlayer")
+		video.finished.connect(self._on_video_finished)
+		video.play()
 
 func _on_video_finished():
-        get_tree().change_scene_to_file("res://night.tscn")
+		get_tree().change_scene_to_file("res://night.tscn")
